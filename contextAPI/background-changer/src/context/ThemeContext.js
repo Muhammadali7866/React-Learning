d7e36d22546eff1ed24a1ed5b30e18fd15246564
Muhammadali7866 <malikali8157@gmail.com>
2025-04-29 18:12:24 +0500

    m-ali create bacground changer

---
+++ contextAPI/background-changer/src/context/ThemeContext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext} from "react"
+
+// 1) theme context 
+export const  ThemeContext = createContext({
+    themeMode:"light",
+    darkTheme:()=>{},
+    lightTheme:()=>{}
+})
+
+
+// 2) use context Provider
+export const ThemeProbvider = ThemeContext.Provider;
+
+
+
+// 3) use THeme 
+export default function useTheme() {
+    return useContext(ThemeContext)
+}
+
+
+
+
+
